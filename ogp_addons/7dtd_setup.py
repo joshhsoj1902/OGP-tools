a45a5cfe390f50f188bb41f08b9abd273f4b900b
Josh Bryans <joshhsoj1902@gmail.com>
2015-01-11 19:33:14 +0000

    Added sample file for 7dtd, this addon does not currently do anything

---
+++ ogp_addons/7dtd_setup.py
@@ -0,0 +1,5 @@
+i#/usr/bin/python
+import insert_common
+
+#This insert is useless at the moment. 
+insert_common.insert_addon("Testing addon","www.google.ca","/tmp/","plugin","warsow_win32","touch /tmp/installed.tmp;");
