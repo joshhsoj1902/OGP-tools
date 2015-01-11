a66b1f5b6aabe3a406ef0aa633ed7dd9c334d646
Josh Bryans <joshhsoj1902@gmail.com>
2015-01-11 22:20:43 +0000

    fixed bootstrap runner to support creating the folder if needed

--- 7dtd/7dtd_bootstrap_runner.sh
+++ 7dtd/7dtd_bootstrap_runner.sh
@@ -1,2 +1,5 @@
-bash ogp_7dtd_bootstrap.sh -i -u ogpuser -g users -p /home/ogpuser/OGP_User_Files/7dtd;
+#!/bin/bash
+
+mkdir -p /home/ogpuser/OGP_User_Files/7dtd;
+bash /usr/local/bin/OGP-tools/7dtd/ogp_7dtd_bootstrap.sh -i -u ogpuser -g users -p /home/ogpuser/OGP_User_Files/7dtd;
 . /etc/bash_completion;
