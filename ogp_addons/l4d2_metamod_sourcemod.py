28eab6246ccd8036676e0d17dfed0048c15ddb5e
Josh Bryans <joshhsoj1902@gmail.com>
2015-01-16 01:56:21 +0000

    removed references to csgo in commands

--- ogp_addons/l4d2_metamod_sourcemod.py
+++ ogp_addons/l4d2_metamod_sourcemod.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python
+##
+#http://opengamepanel.org/forum/viewthread.php?thread_id=2243
+##
+
 import insert_common
 
-post_bash="""cd %home_path%/csgo
+post_bash="""cd %home_path%/left4dead2
 wget http://www.gsptalk.com/mirror/sourcemod/mmsource-1.10.4-linux.tar.gz
 tar -zxvf mmsource-1.10.4-linux.tar.gz
-echo "\"Plugin\"">%home_path%/csgo/addons/metamod.vdf
-echo "{">>%home_path%/csgo/addons/metamod.vdf
-echo    "\"file\"   \"../csgo/addons/metamod/bin/server\"">>%home_path%/csgo/addons/metamod.vdf
-echo "}">>%home_path%/csgo/addons/metamod.vdf
-rm -f %home_path%/csgo/addons/metamod/sourcemod.vdf
-echo "sm addons/sourcemod/bin/sourcemod_mm">>%home_path%/csgo/addons/metamod/metaplugins.ini"""
-
-
-
-
+echo "\"Plugin\"">%home_path%/left4dead2/addons/metamod.vdf
+echo "{">>%home_path%/left4dead2/addons/metamod.vdf
+echo    "\"file\"   \"../left4dead2/addons/metamod/bin/server\"">>%home_path%/left4dead2/addons/metamod.vdf
+echo "}">>%home_path%/left4dead2/addons/metamod.vdf
+rm -f %home_path%/left4dead2/addons/metamod/sourcemod.vdf
+echo "sm addons/sourcemod/bin/sourcemod_mm">>%home_path%/left4dead2/addons/metamod/metaplugins.ini"""
 
 insert_common.insert_addon(
-    "Metamod Source 1.10.4 + Sourcemod 1.6.3",
+    "Metamod Source [1.10.4] + Sourcemod [1.6.3]",
     "http://cdn.probablyaserver.com/sourcemod/sourcemod-1.6.3-linux.tar.gz",
     "left4dead2",
     "plugin",
