47d1d04fbcbcef76a944ba62c203082576d2de77
Josh Bryans <joshhsoj1902@gmail.com>
2015-03-01 15:03:08 +0000

    Install scripts for Terraria

--- terraria/install_mono.sh
+++ terraria/install_mono.sh
@@ -1,4 +1,7 @@
 #Maybe MONO should be installed on my servers by default?
+#
+#To install the latest version see here:
+#http://www.mono-project.com/docs/getting-started/install/linux/#debian-ubuntu-and-derivatives
 sudo apt-get -y install mono-complete;
 sudo apt-get -y install mono-runtime;
 sudo apt-get -y install libmono-system-core4.0-cil
---
+++ terraria/install_terraria_TShock_4.2.6.sh
@@ -0,0 +1,2 @@
+wget https://github.com/NyxStudios/TShock/releases/download/v4.2.6/tshock_release.zip;
+unzip tshock_release.zip;
