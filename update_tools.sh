58b51796bc3d204d14cf0474b19ebf5b554db171
Josh Bryans <joshhsoj1902@gmail.com>
2015-03-08 16:29:13 +0000

    Additonal displays

--- update_tools.sh
+++ update_tools.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 git pull origin;
+echo "============="
 echo "Tools Updated"
+echo "============="
 
 find -L /var/www/modules/config_games/server_configs/ -type l -delete;
 
 ln -sf /usr/local/bin/OGP-tools/www/modules/config_games/server_configs/* /var/www/modules/config_games/server_configs/;
+echo "======================"
 echo "Server Configs Updated"
+echo "======================"
+
+echo "===="
+echo "Done"
+echo "===="
