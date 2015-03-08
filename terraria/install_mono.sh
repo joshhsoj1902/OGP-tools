01498038e2d0b8f94238f0da38e6f9351d87e618
Josh Bryans <joshhsoj1902@gmail.com>
2015-03-08 17:27:16 +0000

    Updated script to install the latest mono

--- terraria/install_mono.sh
+++ terraria/install_mono.sh
@@ -2,7 +2,11 @@
 #
 #To install the latest version see here:
 #http://www.mono-project.com/docs/getting-started/install/linux/#debian-ubuntu-and-derivatives
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF;
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list;
+sudo apt-get update;
+
 sudo apt-get -y install mono-complete;
 sudo apt-get -y install mono-runtime;
 sudo apt-get -y install libmono-system-core4.0-cil
-
