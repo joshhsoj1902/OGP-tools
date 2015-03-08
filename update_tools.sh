768fbc499c48bdf5b74f6f12fad5c76ae0a34ff6
root <root@ogp.joshbryans.me>
2015-03-08 16:14:26 +0000

    Added additonal printouts to update

--- update_tools.sh
+++ update_tools.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 git pull origin;
+echo "Tools Updated"
 
 find -L /var/www/modules/config_games/server_configs/ -type l -delete;
 
 ln -sf /usr/local/bin/OGP-tools/www/modules/config_games/server_configs/* /var/www/modules/config_games/server_configs/;
+echo "Server Configs Updated"
