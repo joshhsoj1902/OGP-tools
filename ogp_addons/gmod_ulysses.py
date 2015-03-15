f5da9283e9c9feb7cae68a795bf235cd1fbfba5d
Josh Bryans <joshhsoj1902@gmail.com>
2015-03-15 20:53:52 +0000

    Initial installer for ULib addon

---
+++ ogp_addons/gmod_ulysses.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+##
+#http://ulyssesmod.net/downloads.php
+##
+
+import insert_common
+
+post_bash="""cd %home_path%/garrysmod/addons
+wget http://ulyssesmod.net/archive/ulx/ulx-v3_62.zip 
+unzip ulx-v3_62.zip
+rm -f ulx-v3_62.zip
+rm -f ULib-v2_52.zip
+"""
+
+insert_common.insert_addon(
+    "ULib [2.52] + ULX [3.62]",
+    "http://ulyssesmod.net/archive/ULib/ULib-v2_52.zip",
+    "garrysmod/addons",
+    "plugin",
+    "GarrysMod_linux32",
+    post_bash);
