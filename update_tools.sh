d3a896b011373521e096c3048cbdaee1db0c761d
Josh Bryans <joshhsoj1902@gmail.com>
2015-03-18 23:18:26 +0000

    Add initial local Rsync server support

--- update_tools.sh
+++ update_tools.sh
@@ -12,6 +12,23 @@ echo "======================"
 echo "Server Configs Updated"
 echo "======================"
 
+#Temporary internal rsync server
+echo "192.168.0.103" > /var/www/modules/gamemanager/rsync_sites_local.list
+
+#It would be better if there was some way to know which games were added by me.
+#this method doesn't support removing games later. It can only add games and 
+#remove games before adding them right away again
+while read p; do
+  sed -i '/'$p'/d' /var/www/modules/gamemanager/rsync.list
+  echo $p >> /var/www/modules/gamemanager/rsync.list
+done <rsync_games.txt
+
+
+
+echo "============="
+echo "Rsync Updated"
+echo "============="
+
 echo "===="
 echo "Done"
 echo "===="
