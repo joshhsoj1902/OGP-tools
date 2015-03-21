1f988a1ec1631e808686a2789f4ea48e21c0bdd4
Josh Bryans <joshhsoj1902@gmail.com>
2015-03-21 19:49:55 +0000

    Change OpenTTD to launch via launcher script to handle clean save & shutdown.

---
+++ openttd/openttd_launcher.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+SESSION_NAME=openttd$(date +%m%d%Y_%H%M%S)
+PARAMS=""
+
+while [[ $# > 1 ]]
+do
+key="$1"
+
+case $key in
+    -g)
+	echo $2
+	if [ ! -f $2 ]; then
+    		echo "File not found!"
+	else
+		PARAMS="$PARAMS $key $2"
+	fi
+    #EXTENSION="$2"
+    shift
+    ;;
+    *)
+	PARAMS="$PARAMS $key"
+	echo $key
+            # unknown option
+    ;;
+esac
+shift
+done
+
+echo $PARAMS
+
+screen -A -m -d -S $SESSION_NAME ./openttd $PARAMS 
+
+trap bashtrap EXIT
+bashtrap()
+{
+        echo "shutdown"
+        screen -S $SESSION_NAME -X stuff 'save save/openttd'`echo -ne '\015'`
+        screen -S $SESSION_NAME -X stuff 'quit'`echo -ne '\015'`
+}
+
+while :                
+do
+        sleep 60
+done
+
+exit 0
+
